916e787e92c64b7370e77b72c0f19cf9aad42c23
jrierapeiro <jrierapeiro@ancestry.com>
2017-12-04 16:19:31 +0000

    Added ec2 jenkins terraform with nodejs

---
+++ jenkinx_deployment/README.md
@@ -0,0 +1,8 @@
+# Terraform - AWS 
+
+Configure a VPC with IGW.
+Configure Public subnet.
+Configure Route tables for the public subnet.
+Configure secuity group.
+Attach an existing development role to the EC2 isntance.
+Configure linux ec2 instance with a user data script to install Jenkins with nodejs.
--- jenkinx_deployment/main.tf
+++ jenkinx_deployment/main.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "testing_vpc" {
+resource "aws_vpc" "con_dep_vpc" {
     cidr_block = "${var.vpc_cidr}"
     enable_dns_hostnames = true
     tags {
@@ -6,12 +6,12 @@ resource "aws_vpc" "testing_vpc" {
     }
 }
 
-resource "aws_internet_gateway" "testing_igw" {
-    vpc_id = "${aws_vpc.testing_vpc.id}"
+resource "aws_internet_gateway" "con_dep_igw" {
+    vpc_id = "${aws_vpc.con_dep_vpc.id}"
 }
 
-resource "aws_subnet" "testing_eu-west-1a-public" {
-    vpc_id = "${aws_vpc.testing_vpc.id}"
+resource "aws_subnet" "con_dep_eu-west-1a-public" {
+    vpc_id = "${aws_vpc.con_dep_vpc.id}"
 
     cidr_block = "${var.public_subnet_cidr}"
     availability_zone = "eu-west-1a"
@@ -21,12 +21,12 @@ resource "aws_subnet" "testing_eu-west-1a-public" {
     }
 }
 
-resource "aws_route_table" "testing_eu-west-1a-public_route_table" {
-    vpc_id = "${aws_vpc.testing_vpc.id}"
+resource "aws_route_table" "con_dep_eu-west-1a-public_route_table" {
+    vpc_id = "${aws_vpc.con_dep_vpc.id}"
 
     route {
         cidr_block = "0.0.0.0/0"
-        gateway_id = "${aws_internet_gateway.testing_igw.id}"
+        gateway_id = "${aws_internet_gateway.con_dep_igw.id}"
     }
 
     tags {
@@ -35,17 +35,18 @@ resource "aws_route_table" "testing_eu-west-1a-public_route_table" {
 }
 
 resource "aws_route_table_association" "eu-west-1a-public" {
-    subnet_id = "${aws_subnet.testing_eu-west-1a-public.id}"
-    route_table_id = "${aws_route_table.testing_eu-west-1a-public_route_table.id}"
+    subnet_id = "${aws_subnet.con_dep_eu-west-1a-public.id}"
+    route_table_id = "${aws_route_table.con_dep_eu-west-1a-public_route_table.id}"
 }
 
-resource "aws_security_group" "testing_sg_web" {
+
+resource "aws_security_group" "con_dep_sg_web" {
     name = "public_sg"
     description = "Allow traffic from internet web on port 3000 and ssh"   
 
     ingress {
-        from_port = 3000
-        to_port = 3000
+        from_port = 8080
+        to_port = 8080
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
@@ -78,8 +79,8 @@ resource "aws_security_group" "testing_sg_web" {
     }
     
     egress {
-        from_port = 3000
-        to_port = 3000
+        from_port = 8080
+        to_port = 8080
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
@@ -111,15 +112,15 @@ resource "aws_security_group" "testing_sg_web" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
-    vpc_id = "${aws_vpc.testing_vpc.id}"
+    vpc_id = "${aws_vpc.con_dep_vpc.id}"
 
     tags {
         Name = "Public Web SG"
     }
 }
 
-data "template_file" "linux_user_data" {
-  template = "${file("linux_user_data.txt")}"  
+data "template_file" "jenkins_user_data" {
+  template = "${file("jenkins_user_data.txt")}"  
 }
 
 resource "aws_instance" "linux-ec2" {
@@ -127,12 +128,13 @@ resource "aws_instance" "linux-ec2" {
     availability_zone = "eu-west-1a"
     instance_type = "t2.micro"
     key_name = "${var.aws_key_name}"
-    vpc_security_group_ids = ["${aws_security_group.testing_sg_web.id}"]
-    subnet_id = "${aws_subnet.testing_eu-west-1a-public.id}"
+    vpc_security_group_ids = ["${aws_security_group.con_dep_sg_web.id}"]
+    subnet_id = "${aws_subnet.con_dep_eu-west-1a-public.id}"
     associate_public_ip_address = true
-    user_data = "${data.template_file.linux_user_data.rendered}"
-
+    user_data = "${data.template_file.jenkins_user_data.rendered}"
+    iam_instance_profile="Deployment-role"
+    
     tags {
-        Name = "Linux EC2"
+        Name = "Jenkins EC2"
     }
 }
